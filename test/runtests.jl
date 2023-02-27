bdea7ccf2dd9b6e1015ed23747c1fcf072eae5c0
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-02-27 17:36:14 +0100

    add l2 tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Test: @test, @testset, @inferred
 using Random: bitrand
 using Statistics: mean
+using LinearAlgebra: I
 using BinaryLogisticRegression: log_likelihood, log_likelihood_gradient, log_likelihood_hessian,
     logistic_regression_without_bias, logistic_regression
 using Zygote: gradient, hessian
@@ -9,17 +10,18 @@ using LogExpFunctions: logistic
 X = randn(7, 100)
 y = bitrand(100)
 w = randn(7)
+l2 = rand()
 
 gs = gradient(w) do w
-    log_likelihood(w, X, y)
+    log_likelihood(w, X, y) - l2 * sum(w.^2) / 2
 end
 
 H = hessian(w) do w
-    log_likelihood(w, X, y)
+    log_likelihood(w, X, y) - l2 * sum(w.^2) / 2
 end
 
-@test @inferred(log_likelihood_gradient(w, X, y)) ≈ only(gs)
-@test @inferred(log_likelihood_hessian(w, X, y)) ≈ H
+@test @inferred(log_likelihood_gradient(w, X, y) - l2 * w) ≈ only(gs)
+@test @inferred(log_likelihood_hessian(w, X, y) - l2 * I) ≈ H
 
 @test logistic_regression_without_bias(X, y; algorithm=:lbfgs) ≈ logistic_regression_without_bias(X, y; algorithm=:newton) rtol=1e-4
 
