a3398289e59c7e08c9076225f34770ded89219d3
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-02-27 17:30:08 +0100

    logistic_regression test

--- src/logistic.jl
+++ src/logistic.jl
@@ -1,7 +1,8 @@
 
-function logistic_regression(X::AbstractMatrix, y::AbstractVector{Bool}; l2::Real = 0)
+function logistic_regression(X::AbstractMatrix, y::AbstractVector{Bool}; l2::Real = 0, algorithm::Symbol=:newton)
     @assert size(X, 2) == length(y)
-    _w = logistic_regression_without_bias([X ; ones(eltype(X), size(X, 2))], y; l2)
+    _X = [X ; ones(eltype(X), 1, size(X, 2))]
+    _w = logistic_regression_without_bias(_X, y; l2, algorithm)
     w = _w[1:end - 1]
     b = _w[end]
     return w, b
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using Test: @test, @testset, @inferred
 using Random: bitrand
+using Statistics: mean
 using BinaryLogisticRegression: log_likelihood, log_likelihood_gradient, log_likelihood_hessian,
-    logistic_regression_without_bias
+    logistic_regression_without_bias, logistic_regression
 using Zygote: gradient, hessian
+using LogExpFunctions: logistic
 
 X = randn(7, 100)
 y = bitrand(100)
@@ -20,3 +22,7 @@ end
 @test @inferred(log_likelihood_hessian(w, X, y)) ≈ H
 
 @test logistic_regression_without_bias(X, y; algorithm=:lbfgs) ≈ logistic_regression_without_bias(X, y; algorithm=:newton) rtol=1e-4
+
+w, b = logistic_regression(X, y)
+@test mean(logistic.(X' * w .+ b)) ≈ mean(y)
+@test X * y ≈ X * logistic.(X' * w .+ b) rtol=1e-5
